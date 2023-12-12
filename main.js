5effece3b2074aae2c97892c687ff7fbdb4ca501
Ishita Sharma <110284120+IshitaSharma123@users.noreply.github.com>
2023-12-12 22:52:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+smileX = 0;
+smileY = 0;
+
+function preload(){
+    smile = loadImage('https://i.postimg.cc/vmv7fLsr/smile.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        console.log("nose x: " + results[0].pose.nose.x);
+        smileX = results[0].pose.nose.x-30;
+        smileY = results[0].pose.nose.y;
+        console.log("nose y: " + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(smile, smileX, smileY, 60, 40);
+}
+
+function take_snapshot() {
+    save("My_Picture.jpg");
+}
